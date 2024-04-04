6c11464404f8c6abdd3833fbf759cf5624e59cf2
InhumanHuman <98548819+InhumanHuman@users.noreply.github.com>
2024-04-04 15:27:40 +0200

    test1

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -16,3 +16,37 @@ describe('Swiper Gallery Test', function () {
     cy.get('.swiper-slide-active').should('contain', 'Paris');
   });
 });
+
+describe('Swiper Gallery Test', function () {
+  it('Przewijanie slajdow w galerii za pomoca przyciskow nawigacji', function () {
+    cy.visit('http://localhost:3000');
+
+    cy.get('.swiper-button-next').click();
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('exist');
+
+    cy.get('.swiper-button-prev').click();
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('exist');
+  });
+});
+
+describe('Swiper Gallery Test', function () {
+  it('czy opis każdego slajdu jest wyświetlany poprawnie', function () {
+    cy.visit('http://localhost:3000');
+
+    cy.get('.swiper-button-next').click();
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('contain', 'United Kingdom');
+
+    cy.get('.swiper-button-prev').click();
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('contain', 'Italy');
+
+    cy.get('.swiper-button-next').click();
+    cy.wait(2000);
+    cy.get('.swiper-button-next').click({ force: true });
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('contain', 'France');
+  });
+});
